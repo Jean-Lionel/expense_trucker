d7e71ebc50a064b0cf455e4d93d67e3584fb8a53
Jean-Lionel <nijeanlionel@gmail.com>
2023-07-05 09:37:40 +0200

    adding amount controller

--- lib/widget/new_expensive.dart
+++ lib/widget/new_expensive.dart
@@ -8,18 +8,51 @@ class NewExpensive extends StatefulWidget {
 }
 
 class _NewExpensiveState extends State<NewExpensive> {
+  final _titleController = new TextEditingController();
+  final _amountController = new TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(8.0),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               label: Text("Title"),
             ),
           ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              label: Text("Amount"),
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Cancel'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountController.text);
+                },
+                child: const Text('Save Expensive'),
+              )
+            ],
+          )
         ],
       ),
     );
