396045e24f246ec8ab442bd5b923e435300b6831
Jean-Lionel <nijeanlionel@gmail.com>
2023-07-06 05:40:52 +0200

    import chart and chart bart

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -42,3 +42,23 @@ class Expense {
     return iconCategories[category];
   }
 }
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+
+  ExpenseBucket.forCategory(List<Expense> allExpensive, this.category)
+      : expenses = allExpensive
+            .where((element) => element.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+    for (var expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
+  }
+}
